a72c8a5e78f991fee85f16fd36a08a25ce197af3
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-11-23 18:37:00 -0300

    feat: Add new use cases - validate alias - access event - save event - save guest

--- apps/backend/src/events/events.controller.ts
+++ apps/backend/src/events/events.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { Event, EventDate, events_seed, Id } from 'core';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  complementEvent,
+  complementGuest,
+  Event,
+  EventDate,
+  events_seed,
+  Guest,
+  Id,
+} from 'core';
 
 @Controller('events')
 export class EventsController {
@@ -21,6 +29,13 @@ export class EventsController {
     }
   }
 
+  @Get('/validate/:alias/:id')
+  async validateAlias(@Param('alias') alias: string, @Param('id') id: string) {
+    const event = events_seed.find((event) => event.alias === alias);
+
+    return { valid: !event || event.id === id };
+  }
+
   private serialize(event: Event) {
     if (!event) return null;
 
@@ -30,6 +45,19 @@ export class EventsController {
     };
   }
 
+  @Post('/access')
+  async accessEvent(@Body() data: { id: string; password: string }) {
+    const event = events_seed.find(
+      (event) => event.id === data.id && event.password === data.password,
+    );
+
+    if (!event) {
+      throw new Error('Senha não corresponde ao evento.');
+    }
+
+    return this.serialize(event);
+  }
+
   private deserealize(event: any): Event {
     if (!event) return null;
 
@@ -37,5 +65,33 @@ export class EventsController {
       ...event,
       date: EventDate.parse(event.date),
     };
+  }
+
+  @Post(':alias/guest')
+  async saveGuest(@Param('alias') alias: string, @Body() guest: Guest) {
+    const event = events_seed.find((event) => event.alias === alias);
+
+    if (!event) {
+      throw new Error('Senha não corresponde ao evento.');
+    }
+
+    const guestComplete = complementGuest(guest);
+    event.guests.push(guestComplete);
+
+    return this.serialize(event);
+  }
+
+  @Post()
+  async saveEvent(@Body() event: Event) {
+    const eventExists = events_seed.find((ev) => ev.alias === event.alias);
+
+    if (eventExists && eventExists.id !== event.id) {
+      throw new Error('Já existe um evento com este alias.');
+    }
+
+    const eventComplete = complementEvent(this.deserealize(event));
+    events_seed.push(eventComplete);
+
+    return this.serialize(event);
   }
 }
